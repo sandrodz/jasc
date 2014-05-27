768a451340eb193c8dcc348e8622b37dea3047aa
Sandro Dzneladze <sandro@idev.ge>
2014-05-27 16:15:09 +0400

    added input validation

--- jquery.asc.js
+++ jquery.asc.js
@@ -1,7 +1,7 @@
 /*
  * 
  * jQuery Amortization Schedule Calculator
- * By: Sandro Dzneladze [http://idev.ge]
+ * By: Sandro Dzneladze [http://jasc.idev.ge]
  *
  * Copyright 2014 Sandro Dzneladze
  *  
@@ -28,11 +28,12 @@
  */
 
 (function($) {
-    $.fn.amortizationSchedule = function(options) {
+    $.fn.jasc = function(options) {
 
         var settings = $.extend({/* Defaults */},options);
 
         var controlsHtml = '<div class="loan">' +
+                               '<h3>Loan Details</h3>' +
                                '<div class="input-group">' +
                                     '<span class="input-group-addon">Loan Amount</span>' +
                                     '<input type="text" class="form-control" placeholder="e.g. 3800" id="amortization-schedule-amount" />' +
@@ -55,6 +56,7 @@
                                 '</div>' +
                            '</div>' +
                            '<div class="date">' +
+                                 '<h3>Loan Start Date</h3>' +
                                  '<div class="input-group">' +
                                     '<span class="input-group-addon">Start Month</span>' +
                                     '<input type="text" class="form-control" placeholder="e.g. 12" id="amortization-schedule-month" />' +
@@ -71,21 +73,42 @@
                            '<button type="button" class="btn btn-success btn-lg" id="amortization-schedule-submit">Calculate</button>';
         $('#'+settings.controlsID).append(controlsHtml);
 
+        var container = this;
         $('#amortization-schedule-submit').on('click', function(){
-            calculate();
+            var dataHtml = calculate();
+            if (dataHtml) {
+                // Insert into dom
+                $(container).find('table').remove();
+                $(container).append(dataHtml.join(''));
+            }
             return false;
         });
 
-        var me = this;
         function calculate() {
-            var loanAmount = parseInt($('#amortization-schedule-amount').val()),
-                loanInterest = parseInt($('#amortization-schedule-interest').val()), // %
-                loanDuration = parseInt($('#amortization-schedule-duration').val()), // Month
-                loanGrace = parseInt($('#amortization-schedule-grace').val()), // Month
-                loanFee = parseInt($('#amortization-schedule-fee').val()), // %
+            var loanAmount    = parseInt($('#amortization-schedule-amount').val()),
+                loanInterest  = parseInt($('#amortization-schedule-interest').val()), // %
+                loanDuration  = parseInt($('#amortization-schedule-duration').val()), // Month
+                loanGrace     = parseInt($('#amortization-schedule-grace').val()), // Month
                 loanInitMonth = parseInt($('#amortization-schedule-month').val()),
-                loanInitDay = parseInt($('#amortization-schedule-day').val()),
-                loanInitYear = parseInt($('#amortization-schedule-year').val());
+                loanInitDay   = parseInt($('#amortization-schedule-day').val()),
+                loanInitYear  = parseInt($('#amortization-schedule-year').val());
+
+            // validate input
+            if (isNaN(loanAmount) || isNaN(loanInterest) || isNaN(loanDuration) || isNaN(loanGrace) || isNaN(loanInitMonth) || isNaN(loanInitDay) || isNaN(loanInitYear) || 
+                loanAmount < 1 || loanInterest < 0 || loanInterest > 100 || loanDuration < 1 || loanGrace < 0 || loanInitMonth < 0 || loanInitMonth > 12 || loanInitDay < 0 || loanInitDay > 31 || loanInitYear < 1000 || loanInitYear > 9999) {
+                if (isNaN(loanAmount) || loanAmount < 1)                                { $('#amortization-schedule-amount').addClass('error'); } else { $('#amortization-schedule-amount').removeClass('error'); }
+                if (isNaN(loanInterest) || loanInterest < 0 || loanInterest > 100)      { $('#amortization-schedule-interest').addClass('error'); } else { $('#amortization-schedule-interest').removeClass('error'); }
+                if (isNaN(loanDuration) || loanDuration < 1)                            { $('#amortization-schedule-duration').addClass('error'); } else { $('#amortization-schedule-duration').removeClass('error'); }
+                if (isNaN(loanGrace) || loanGrace < 0)                                  { $('#amortization-schedule-grace').addClass('error'); } else { $('#amortization-schedule-grace').removeClass('error'); }
+                if (isNaN(loanInitMonth) || loanInitMonth < 0 || loanInitMonth > 12)    { $('#amortization-schedule-month').addClass('error'); } else { $('#amortization-schedule-month').removeClass('error'); }
+                if (isNaN(loanInitDay) || loanInitDay < 0 || loanInitDay > 31)          { $('#amortization-schedule-day').addClass('error'); } else { $('#amortization-schedule-day').removeClass('error'); }
+                if (isNaN(loanInitYear) || loanInitYear < 1000 || loanInitYear > 9999)  { $('#amortization-schedule-year').addClass('error'); } else { $('#amortization-schedule-year').removeClass('error'); }
+                return false;
+            } else {
+                $.each($('#'+settings.controlsID).find('input'),function(key,val){
+                    $(val).removeClass('error');
+                });
+            }
 
             var loanStart = new Date(loanInitYear,loanInitMonth,loanInitDay);
             var loanTable = [];
@@ -134,9 +157,7 @@
                                               + Math.round(loanTable[t].installment) +'</td><td>'
                                                + roundTo2(loanTable[t].balance) +'</td></tr>' + (t == loanTable.length - 1 ? '</tbody></table>' : ''));
             }
-            // Insert into dom
-            $(me).find('table').remove();
-            $(me).append(loanTableTd.join(''));
+            return loanTableTd;
         }
 
         // Helper functions
